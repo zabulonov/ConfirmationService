a51c4f58c22f835739eed646020ed60ea8bb67ac
Alexey <zabulonov444@yandex.ru>
2024-01-06 15:09:57 +0300

    comment do not working tests

--- ConfirmationService.Tests/UserServiceTests.cs
+++ ConfirmationService.Tests/UserServiceTests.cs
@@ -1,31 +1,31 @@
-using ConfirmationService.BusinessLogic.Services;
-
-namespace ConfirmationService.Tests;
-
-public class UserServiceTests
-{
-    private readonly UserService _service;
-
-    public const string ValidName = "testName";
-
-    // public UserServiceTests(UserService service)
-    // {
-    //     _service = service;
-    // }
-
-    
-    // что это получается, мне нужено сделать мок, Userservice, ConfirmServiceContext, mailSendService что затестить 1 функцию?
-    [SetUp]
-    public void Setup(UserService service)
-    {
-        _service = new UserService();
-    }
-    
-    [TestCase(ValidName)]
-    [Test]
-    public void TestRegisterNewUser_ValidInput(string conpanyName)
-    {
-        
-        Assert.Pass();
-    }
-}
+// using ConfirmationService.BusinessLogic.Services;
+//
+// namespace ConfirmationService.Tests;
+//
+// public class UserServiceTests
+// {
+//     private readonly UserService _service;
+//
+//     public const string ValidName = "testName";
+//
+//     // public UserServiceTests(UserService service)
+//     // {
+//     //     _service = service;
+//     // }
+//
+//     
+//     // что это получается, мне нужено сделать мок, Userservice, ConfirmServiceContext, mailSendService что затестить 1 функцию?
+//     [SetUp]
+//     public void Setup(UserService service)
+//     {
+//         _service = new UserService();
+//     }
+//     
+//     [TestCase(ValidName)]
+//     [Test]
+//     public void TestRegisterNewUser_ValidInput(string conpanyName)
+//     {
+//         
+//         Assert.Pass();
+//     }
+// }

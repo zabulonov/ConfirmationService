80284b934a545f537257bbca49e401126b92b52d
Alexey <zabulonov444@yandex.ru>
2024-02-07 16:03:06 +0300

    Delete old TODO

--- ConfirmationService.BusinessLogic/Services/MailSendService.cs
+++ ConfirmationService.BusinessLogic/Services/MailSendService.cs
@@ -39,7 +39,6 @@ public class MailSendService(MailConnect mailConnectConnect)
         message.Subject = emailModel.Subject;
         
         var builder = new BodyBuilder ();
-        // TODO - Можно ли как то вынести это в appsettings?
         builder.HtmlBody = string.Format("<h2>Dear {0},</h2><h4>To confirm your email, please follow the link:</h4><center><form method=\"get\" action=\"http://localhost:5277/email-confirmation/confirm?\"> <input type=\"hidden\" name=\"token\" value=\"{1}\" /> <input type=\"submit\" value=\"Confirm\" style=\"height:50px; width:100px\" /></form></center><br>Made as a pet project by Alexey Zabulonov<br>\n<a href=\"https://t.me/ulove1337\"><b>Telegram</b></a><br>\n<a href=\"https://github.com/zabulonov\">GitHub</a><br>\n<a href=\"https://www.linkedin.com/in/alexey-zabulonov-442b03283\">LinkedIn</a>", userClientModel.Name, token);
         
         message.Body = builder.ToMessageBody ();
